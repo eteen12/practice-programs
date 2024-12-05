bc4fc4d388bf74315259f9587f67add28556f9e5
eteen12 <eteensmash@gmail.com>
2024-12-04 19:09:10 -0800

    made sort by selection

---
+++ selection-sort/file.js
@@ -0,0 +1,14 @@
+const array = [1, 5, 2, 6, 1, 6, 7, 8, 9];
+
+for (i = 0; i < array.length - 1; i++) {
+  let min = i;
+  for (j = i + 1; j < array.length; j++) {
+    if (array[min] > array[j]) {
+      min = j;
+    }
+  }
+  const temp = array[i];
+  array[i] = array[min];
+  array[min] = temp;
+}
+console.log(array);
